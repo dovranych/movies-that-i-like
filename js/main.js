7103cb0c57886bdca260f86a62b331d809b5b23a
dovranych <146568153+dovranych@users.noreply.github.com>
2023-11-20 17:48:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(window).on('load', function () {
+  $('#header').vide('./video/cover', {
+    bgColor: '#011c15'
+  });
+});
+$(window).on('load', function () {
+  $('#footer').vide('./video/cover', {
+    bgColor: '#011c15'
+  });
+});
